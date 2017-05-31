9ad4ca00fef539b3d1aee98478136f59ca46eb55
Sylvie <sylvie.veyres@gmail.com>
2017-05-31 12:22:26 +0200

    fix css

--- index.php
+++ index.php
@@ -2,10 +2,63 @@
 
     <main>
         <div class="container">
-            <div class="row ">
-                <div class="col-md-3">
-                    POISNHBNLIQSOJFK
-                </div>
+
+            <div class="row baseline">
+                    <?php
+                    $args = array(
+            	           'posts_per_page'   => 3,
+                           'orderby'          => 'date',
+	                       'order'            => 'ASC',
+            	           'category_name'    => 'baseline'
+                    );
+                    $posts = get_posts($args);
+                    get_template_part( 'template-parts/content_loop_baseline');
+                    ?>
+            </div>
+            <div class="row actu">
+                    <div class="col-md-4">
+                        <?php
+
+                        $args = array(
+                            'posts_per_page'   => 3,
+                            'orderby'          => 'date',
+                            'order'            => 'ASC',
+                            'category_name'    => 'innovantes'
+                        );
+                        $posts = get_posts($args);?>
+
+                            <?php
+                            printf( '<h2>%s</h2>', $category->name);?>
+                        
+
+
+                            <?php get_template_part( 'template-parts/content_loop_actu'); ?>
+
+                    </div>
+                    <div class="col-md-4">
+                        <?php
+                        $args = array(
+                            'posts_per_page'   => 3,
+                            'orderby'          => 'date',
+                            'order'            => 'ASC',
+                            'category_name'    => 'entreprise'
+                        );
+                        $posts = get_posts($args);
+                        get_template_part( 'template-parts/content_loop_actu');
+                        ?>
+                    </div>
+                    <div class="col-md-4">
+                        <?php
+                        $args = array(
+                            'posts_per_page'   => 3,
+                            'orderby'          => 'date',
+                            'order'            => 'ASC',
+                            'category_name'    => 'teletravail'
+                        );
+                        $posts = get_posts($args);
+                        get_template_part( 'template-parts/content_loop_actu');
+                        ?>
+                    </div>
             </div>
         </div>
     </main>
