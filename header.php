8ae3fe824b951755da4b329c14ae1effac65823e
salwa <salwaoudad@gmail.com>
2017-05-31 12:27:27 +0200

    add header

--- header.php
+++ header.php
@@ -13,9 +13,47 @@
   </head>
   <body>
     <header>
-        <nav id="menu">
-            <ul>
-                <li><a href="">blablablalba</a></li>
-            </ul>
+        <?php require_once('wp-bootstrap-navwalker.php'); ?>
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <?php
+                    wp_nav_menu( array(
+                        'menu'              => 'navbar_top',
+                        'theme_location'    => 'primary',
+                        'depth'             => 2,
+                        'container'         => 'div',
+                        'container_class'   => 'collapse navbar-collapse',
+                        'container_id'      => 'bs-example-navbar-collapse-1',
+                        'menu_class'        => 'nav navbar-nav',
+                        'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                        'walker'            => new WP_Bootstrap_Navwalker())
+                    );
+                ?>
+            </div>
+        </nav>
+
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+                    <a class="navbar-brand" href="#"><img src="./static/img/brand-white.png" alt=""></a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <?php
+                $args = array (
+                  'container' => 'div',
+                  'container_class' => 'collapse navbar-collapse',
+                  'container_id' => 'bs-example-navbar-collapse-1',
+                  'menu_class' => 'nav navbar-nav',
+                  'menu' => 'navbar_header',
+                  'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                  'walker'            => new WP_Bootstrap_Navwalker()
+                );
+
+                wp_nav_menu($args)
+                ?>
+            </div>
         </nav>
     </header>
