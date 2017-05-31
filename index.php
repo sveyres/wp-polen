526a8adabc8dd7d45b5ee77f2874ca6e87558ada
salwa <salwaoudad@gmail.com>
2017-05-31 09:59:19 +0200

    add index & static & boostrap

--- index.php
+++ index.php
@@ -1,18 +1,55 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Product Launch</title>
-        <link rel="stylesheet" href="<?php echo get_stylesheet_uri()?>" >
-        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-    </head>
-    <body>
-        <header>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Titre de la page</title>
 
-        </header>
-        <main>
-            <h1>Hello </h1>
-        </main>
-    </body>
+    <link rel="stylesheet" href="static/external/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="static/external/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri()?>" >
+  </head>
+  <body>
+    <header>
+        <nav id="menu">
+            <ul>
+                <li><a href=""></a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-3">
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <ul id="social_nav">
+            <li>
+            <a href="#" target="blank"><i class="fa fa-facebook"></i></a>
+            </li>
+            <li>
+            <a href="#" target="blank"><i class="fa fa-twitter"></i></a>
+            </li>
+            <li>
+            <a href="#" target="blank"><i class="fa fa-google-plus-official"></i></a>
+            </li>
+            <li>
+            <a href="#" target="blank"><i class="fa fa-pinterest"></i></a>
+            </li>
+            <li>
+            <a href="#" target="blank"><i class="fa fa-youtube-play"></i></a>
+            </li>
+        </ul>
+        <p>© 2015 Bootstrap <a href="#">About Help Terms Privacy Cookies Ads Info Brand Blog Status Apps Jobs Advertise</a></p>
+    </footer>
+
+    <script src="static/external/jquery/dist/jquery.js"></script>
+    <script src="static/external/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="static/js/script.js"></script>
+  </body>
 </html>
