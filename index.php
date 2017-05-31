ed513bd8f7ca5edc247d21c24811c750e45ae48f
Sylvie <sylvie.veyres@gmail.com>
2017-05-31 09:34:59 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Product Launch</title>
+        <link rel="stylesheet" href="<?php echo get_stylesheet_uri()?>" >
+        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+
+        </header>
+        <main>
+            <h1>Hello </h1>
+        </main>
+    </body>
+</html>
