7b000bd8182994ef2948d002ad33f487d2313c1f
solene <solenantoine@gmail.com>
2017-05-31 16:30:05 +0200

    slider finished

--- footer.php
+++ footer.php
@@ -1,41 +1,42 @@
-<footer>
-    <div class="container">
-        <div class="rowone">
-            <div class="col-xs-12 col-sm-6">
-                <h4>inscription à la newsletter</h4>
-                <form method="post">
-                    <input type="email" name="email">
-                    <?php
-                        if (isset($_POST['email'])) {
-                            $wpdb->insert( 'form',  array(   'email' => $_POST['email'])
-                                            );
-                        } ?>
-                    <button type="submit" name="button">Envoyer</button>
-                </form>
-            </div>
-            <div class="col-xs-12 col-sm-6">
+    <footer>
+        <div class="container">
+            <div class="rowone">
+                <div class="col-xs-12 col-sm-6">
+                    <h4>inscription à la newsletter</h4>
+                    <form method="post">
+                        <input type="email" name="email">
+                        <?php
+                            if (isset($_POST['email'])) {
+                                $wpdb->insert( 'form',  array(   'email' => $_POST['email'])
+                                                );
+                            } ?>
+                        <button type="submit" name="button">Envoyer</button>
+                    </form>
+                </div>
+                <div class="col-xs-12 col-sm-6">
 
-            <h4>agenda</h4>
-            <p>Auncun événememnt</p>
+                <h4>agenda</h4>
+                <p>Auncun événememnt</p>
+                </div>
+            </div>
+            <div class="rowtwo">
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/static/img/polenfoot.jpg';?>" alt="polen">
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <h5> Pôle Lozérien d'Economie Numérique</h5>
+                    <p>Rue du Gévaudan
+                    48000 Mende
+                    Tel : 04 66 45 33 00 - Fax : 04 66 45 33 00
+                    Email : contact@lozere-developpement.com </p>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/static/img/polenfinanceurs.jpg';?>" alt="financeurs">
+                </div>
             </div>
         </div>
-        <div class="rowtwo">
-            <div class="col-xs-12 col-sm-6 col-md-3">
-                <img src="<?php echo get_stylesheet_directory_uri() . '/static/img/polenfoot.jpg';?>" alt="polen">
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-3">
-                <h5> Pôle Lozérien d'Economie Numérique</h5>
-                <p>Rue du Gévaudan
-                48000 Mende
-                Tel : 04 66 45 33 00 - Fax : 04 66 45 33 00
-                Email : contact@lozere-developpement.com </p>
-            </div>
-            <div class="col-xs-12 col-md-6">
-                <img src="<?php echo get_stylesheet_directory_uri() . '/static/img/polenfinanceurs.jpg';?>" alt="financeurs">
-            </div>
-        </div>
-    </div>
-</footer>
+    </footer>
+</div>
 
 <script src="<?php echo get_stylesheet_directory_uri().'/static/external/jquery/dist/jquery.min.js' ; ?>"></script>
 <script src="<?php echo get_stylesheet_directory_uri().'/static/external/bootstrap/dist/js/bootstrap.min.js'; ?>"></script>
